25880c730527d84c7c26124067908f365381dc40
thank1ess <vladonyaroshchuk@gmail.com>
2024-04-19 04:38:58 +0300

    Create Genre model and add DbSet of Genre

--- MovieStore.DataAccess/Data/ApplicationDbContext.cs
+++ MovieStore.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MovieStore.Models;
 
 namespace MovieStore.DataAccess.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
+        
+        public DbSet<Genre> Genres { get; set; }
     }
 }
