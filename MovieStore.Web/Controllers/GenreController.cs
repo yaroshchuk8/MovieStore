40e76df3f125240703bf17508fed4338eab1524b
thank1ess <vladonyaroshchuk@gmail.com>
2024-04-19 11:39:31 +0300

    Add Upsert and Delete functionality for Genre table

--- MovieStore.Web/Controllers/GenreController.cs
+++ MovieStore.Web/Controllers/GenreController.cs
@@ -18,5 +18,79 @@ namespace MovieStore.Web.Controllers
             List<Genre> genreList = _db.Genres.ToList();
             return View(genreList);
         }
-    }
+
+        public IActionResult Upsert(int? id) //Update+Insert
+		{
+            Genre genre;
+
+            //insert
+            if (id == null || id == 0)
+            {
+                genre = new();
+                return View(genre);
+            }
+            
+            
+            //update
+            genre = _db.Genres.Find(id);
+			
+			if (genre == null) 
+			{
+				return NotFound();
+			}
+           
+			return View(genre);
+        }
+
+		[HttpPost]
+		public IActionResult Upsert(Genre genre)
+		{
+			if (ModelState.IsValid)
+			{
+				if (genre.Id == 0)
+				{
+					_db.Genres.Add(genre);
+					// TempData["success"] = "Product created successfuly";
+				}
+				else
+				{
+					_db.Genres.Update(genre);
+					// TempData["success"] = "Product updated successfuly";
+				}
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View(genre);
+			}
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			Genre genre;
+
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			genre = _db.Genres.Find(id);
+
+			if (genre == null)
+			{
+				return NotFound();
+			}
+
+			return View(genre);
+		}
+
+		[HttpPost]
+		public IActionResult Delete(Genre genre)
+		{
+			_db.Genres.Remove(genre);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
 }
