4746e627d96f7a40324ab22a5f0e982838dc1076
thank1ess <vladonyaroshchuk@gmail.com>
2024-04-20 06:02:54 +0300

    Create Movie model

---
+++ MovieStore.Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieStore.Models
+{
+	public class Movie
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Title { get; set; }
+		[Required]
+		public string Description { get; set; }
+		[Required]
+		[Range(1, 1000)]
+		public double Price { get; set; }
+		[ValidateNever]
+		[Display(Name = "Image")]
+		public string ImageUrl { get; set; }
+	}
+}
--- MovieStore.Web/Controllers/GenreController.cs
+++ MovieStore.Web/Controllers/GenreController.cs
@@ -6,7 +6,7 @@ namespace MovieStore.Web.Controllers
 {
     public class GenreController : Controller
     {
-        private ApplicationDbContext _db;
+        private readonly ApplicationDbContext _db;
 
         public GenreController(ApplicationDbContext db)
         {
@@ -19,75 +19,75 @@ namespace MovieStore.Web.Controllers
             return View(genreList);
         }
 
-		public IActionResult Upsert(int? id) //Update+Insert
-		{
-			Genre genre;
+        public IActionResult Upsert(int? id) //Update+Insert
+        {
+            Genre genre;
 
-			//insert
-			if (id == null || id == 0)
-			{
-				genre = new();
-				return View(genre);
-			}
+            //insert
+            if (id == null || id == 0)
+            {
+                genre = new();
+                return View(genre);
+            }
 
-			//update
-			genre = _db.Genres.Find(id);
+            //update
+            genre = _db.Genres.Find(id);
 
-			if (genre == null)
-			{
-				return NotFound();
-			}
+            if (genre == null)
+            {
+                return NotFound();
+            }
 
-			return View(genre);
-		}
+            return View(genre);
+        }
 
-		[HttpPost]
-		public IActionResult Upsert(Genre genre)
-		{
-			if (ModelState.IsValid)
-			{
-				if (genre.Id == 0)
-				{
-					_db.Genres.Add(genre);
-					// TempData["success"] = "Product created successfuly";
-				}
-				else
-				{
-					_db.Genres.Update(genre);
-					// TempData["success"] = "Product updated successfuly";
-				}
-				_db.SaveChanges();
-				return RedirectToAction("Index");
-			}
-			else
-			{
-				return View(genre);
-			}
-		}
+        [HttpPost]
+        public IActionResult Upsert(Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                if (genre.Id == 0)
+                {
+                    _db.Genres.Add(genre);
+                    // TempData["success"] = "Product created successfuly";
+                }
+                else
+                {
+                    _db.Genres.Update(genre);
+                    // TempData["success"] = "Product updated successfuly";
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(genre);
+            }
+        }
 
-		public IActionResult Delete(int? id)
-		{
-			if (id == null || id == 0)
-			{
-				return NotFound();
-			}
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
 
-			Genre genre = _db.Genres.Find(id);
+            Genre genre = _db.Genres.Find(id);
 
-			if (genre == null)
-			{
-				return NotFound();
-			}
+            if (genre == null)
+            {
+                return NotFound();
+            }
 
-			return View(genre);
-		}
+            return View(genre);
+        }
 
-		[HttpPost]
-		public IActionResult Delete(Genre genre)
-		{
-			_db.Genres.Remove(genre);
-			_db.SaveChanges();
-			return RedirectToAction("Index");
-		}
-	}
+        [HttpPost]
+        public IActionResult Delete(Genre genre)
+        {
+            _db.Genres.Remove(genre);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
 }
