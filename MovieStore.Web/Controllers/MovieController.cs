2fd3a4e4b8cf2eb0c5ce4877e10e7879508957f4
thank1ess <vladonyaroshchuk@gmail.com>
2024-04-22 13:37:37 +0300

    Add MovieController and View to display data in a table

---
+++ MovieStore.Web/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStore.DataAccess.Data;
+using MovieStore.Models;
+
+namespace MovieStore.Web.Controllers
+{
+	public class MovieController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+		public MovieController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public IActionResult Index()
+		{
+			List<Movie> movies = _db.Movies.ToList();
+			return View(movies);
+		}
+	}
+}
