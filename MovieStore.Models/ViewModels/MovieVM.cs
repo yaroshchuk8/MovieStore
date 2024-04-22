e77ad14b63cae8bf1b2066471b14b00e7c0a1804
thank1ess <vladonyaroshchuk@gmail.com>
2024-04-22 13:47:40 +0300

    Add Movie ViewModel to save intermediate data

---
+++ MovieStore.Models/ViewModels/MovieVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MovieStore.Models.ViewModels
+{
+	public class MovieVM
+	{
+		public Movie Movie { get; set; }
+		[ValidateNever]
+		public IEnumerable<SelectListItem> GenreList { get; set; }
+		[ValidateNever]
+		public List<string> SelectedItems { get; set; }
+	}
+}
