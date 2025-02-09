3c48efd3135a580a78be06d38644b6dd2a58ec3f
yaroshchuk8 <vladonyaroshchuk@gmail.com>
2025-02-09 10:28:53 +0200

    Remove unnecessary files. Add Base Api Controller.

---
+++ src/MovieStore.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieStore.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BaseApiController : ControllerBase
+{
+    
+}
--- src/MovieStore.Domain/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MovieStore.Domain;
-
-public class Class1
-{
-
-}
