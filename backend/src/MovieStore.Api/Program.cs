6a57ef6f6c391fe9c8f3dc982add5af56aca5eb0
yaroshchuk8 <vladonyaroshchuk@gmail.com>
2025-02-13 14:24:02 +0200

    Add Cors Policy. Start frontend app on port 3000.

--- backend/src/MovieStore.Api/Program.cs
+++ backend/src/MovieStore.Api/Program.cs
@@ -2,29 +2,34 @@ using MovieStore.Application;
 using MovieStore.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-builder.Services.AddOpenApi();
-
-builder.Services
-    .AddApplication()
-    .AddInfrastructure();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    builder.Services.AddControllers();
+    builder.Services.AddOpenApi();
+    
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
+    
+    builder.Services.AddCors(opt =>
+    {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000");
+        });
+    });
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    app.UseCors("CorsPolicy");
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    
+    app.Run();
+}
