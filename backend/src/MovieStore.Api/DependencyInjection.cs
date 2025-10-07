c129620b8172d4aa6767f8cb06ee4041c328c8bf
yaroshchuk8 <vladonyaroshchuk@gmail.com>
2025-10-07 17:33:06 +0200

    Add Authentication using JWT.

--- backend/src/MovieStore.Api/DependencyInjection.cs
+++ backend/src/MovieStore.Api/DependencyInjection.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using MovieStore.Api.Configuration;
 using MovieStore.Application.Common.Extensions;
 using MovieStore.Infrastructure.Configuration;
@@ -8,7 +11,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApi(this IServiceCollection services, IConfiguration configuration)
     {
-        return services.AddCorsPolicy(configuration);
+        return services
+            .AddCorsPolicy(configuration)
+            .AddJwtAuthentication(configuration);
     }
     
     private static IServiceCollection AddCorsPolicy(this IServiceCollection services, IConfiguration configuration)
@@ -32,7 +37,41 @@ public static class DependencyInjection
         // add strongly-typed configuration class to DI container for Infrastructure Auth service to use
         services.AddSingleton(jwtSettings); 
         
+        var key = Encoding.ASCII.GetBytes(jwtSettings.Secret);
         
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = jwtSettings.Audience,
+                    ValidateLifetime = true
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        Console.WriteLine($"AUTH FAILED: {context.Exception.Message}");
+                        return Task.CompletedTask;
+                    },
+                    OnTokenValidated = _ =>
+                    {
+                        Console.WriteLine("TOKEN VALIDATED");
+                        return Task.CompletedTask;
+                    }
+                };
+            });
         
         return services;
     }
