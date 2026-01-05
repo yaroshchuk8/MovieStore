2a071c36b5488630c10ee726e64a1bf8127e34c2
yaroshchuk8 <vladonyaroshchuk@gmail.com>
2026-01-05 20:18:35 +0100

    Update seeder to create admin user.

--- backend/src/MovieStore.Infrastructure/Common/Services/DbInitializer.cs
+++ backend/src/MovieStore.Infrastructure/Common/Services/DbInitializer.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MovieStore.Application.Users.Interfaces;
 using MovieStore.Domain.Enums;
 using MovieStore.Infrastructure.Common.Persistence;
 using MovieStore.Infrastructure.Common.Services.Interfaces;
@@ -10,6 +11,7 @@ namespace MovieStore.Infrastructure.Common.Services;
 public class DbInitializer(
     UserManager<IdentityUserEntity> userManager,
     RoleManager<IdentityRoleEntity> roleManager,
+    IIdentityService identityService,
     MovieStoreDbContext context)
     : IDbInitializer
 {
@@ -27,6 +29,18 @@ public class DbInitializer(
             {
                 await roleManager.CreateAsync(new IdentityRoleEntity(role));
             }
+        }
+
+        const string adminEmail = "admin@gmail.com";
+        var adminExists = await userManager.Users.AnyAsync(u => u.NormalizedEmail == userManager.NormalizeEmail(adminEmail));
+        if (!adminExists)
+        {
+            await identityService.CreateUserAsync(
+                email: adminEmail,
+                password: "Password1_",
+                name: null,
+                sex: null,
+                role: Role.Admin);
         }
     }
 }
