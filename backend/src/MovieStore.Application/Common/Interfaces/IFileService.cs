051edc2f928c377156a0129438a3d5b8837bb39b
yaroshchuk8 <vladonyaroshchuk@gmail.com>
2026-01-13 23:37:11 +0100

    Update file service.

--- backend/src/MovieStore.Application/Common/Interfaces/IFileService.cs
+++ backend/src/MovieStore.Application/Common/Interfaces/IFileService.cs
@@ -2,5 +2,5 @@ namespace MovieStore.Application.Common.Interfaces;
 
 public interface IFileService
 {
-    Task<string> UploadFileAsync(Stream file, string webRootPath, string fileExtension);
+    Task<string> UploadFileAsync(Stream file, string fileExtension);
 }
--- backend/src/MovieStore.Infrastructure/Common/Services/FileService.cs
+++ backend/src/MovieStore.Infrastructure/Common/Services/FileService.cs
@@ -1,20 +1,23 @@
+using Microsoft.Extensions.Options;
 using MovieStore.Application.Common.Interfaces;
+using MovieStore.Infrastructure.Common.Configurations;
 
 namespace MovieStore.Infrastructure.Common.Services;
 
-public class FileService : IFileService
+public class FileService(IOptions<FileStorageSettings> fileStorageOptions) : IFileService
 {
-    private const string ImagesFolder = "images";
+    private readonly FileStorageSettings _fileStorageSettings = fileStorageOptions.Value;
     
-    public async Task<string> UploadFileAsync(Stream file, string webRootPath, string fileExtension)
+    public async Task<string> UploadFileAsync(Stream file, string fileExtension)
     {
-        var fileName = Guid.NewGuid() + fileExtension;
-        var filePath = Path.Combine(ImagesFolder, fileName);
-        var uploadPath = Path.Combine(webRootPath, filePath);
+        var timestamp = DateTime.UtcNow.ToString("yyyyMMddHHmmssfff");
+        var uniqueId = Guid.NewGuid().ToString("N");
+        var fileFullName = $"{timestamp}_{uniqueId}{fileExtension}";
+        var uploadPath = Path.Combine(_fileStorageSettings.FolderPath, fileFullName);
         
         await using var fileStreamOutput = new FileStream(uploadPath, FileMode.Create);
         await file.CopyToAsync(fileStreamOutput);
 
-        return filePath;
+        return fileFullName;
     }
 }
